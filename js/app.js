f8ad39c0a1cf6e43c8ef590e49bd69f9dd550d76
gmhartman <108906460+gmhartman@users.noreply.github.com>
2022-08-21 15:25:33 -0500

    fixed mobile view and caption search

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ desc = document.getElementsByTagName('a');
 filterSearch.addEventListener("input", e => {
     value = e.target.value.toUpperCase();
     for (i = 0; i < desc.length; i++) {
-        if (!desc[i].innerHTML.toUpperCase().includes(value)) {
+        if (!desc[i].dataset.caption.toUpperCase().includes(value)) {
             desc[i].style.display = 'none';
         } else {
             desc[i].style.display = "";
